76b977fd198839ff036f13f81ec900c0e9d6c0dd
Lloyd Ntim <lloyd.ntim@gmail.com>
2023-06-13 11:10:57 +0200

    restyled about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,17 @@
-import React, { FC } from 'react';
-import { Page } from '../../layouts';
-import { Text, Header, Body, Footer, Headline } from '../../components';
+import React, {FC} from 'react';
+import {Page} from '../../layouts';
+import {Text, Header, Body, Footer, Headline, Box} from '../../components';
 
 const About: FC = () => {
   return (
     <Page name="about">
       <Header />
-      <Body>
-        <Headline>About Us</Headline>
-        <Text>About us text</Text>
+      <Body flex-col align-c flex="1">
+        <Headline tKey="about:title" />
+
+        <Box flex="1">
+          <Text tKey="about:description" />
+        </Box>
       </Body>
       <Footer startYear={2019} companyName="LNCD" />
     </Page>
