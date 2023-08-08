5885b4e74383ce66c1eec37de8b749d88ff7a23d
Lloyd Ntim <lloyd.ntim@gmail.com>
2023-08-08 15:23:16 +0000

    updated styled components package and made adjustments to unit tests and existing components

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,32 +3,44 @@ import {getUnit} from '../../helpers/createStyledProps';
 import Box from '../Box/Box';
 
 interface TableCellProps {
-  ['valign-t']?: boolean;
-  ['valign-m']?: boolean;
-  ['valign-b']?: boolean;
+  '$valign-t'?: boolean;
+  '$valign-m'?: boolean;
+  '$valign-b'?: boolean;
 
-  ['txt-align-l']?: boolean;
-  ['txt-align-c']?: boolean;
-  ['txt-align-r']?: boolean;
+  '$txt-align-l'?: boolean;
+  '$txt-align-c'?: boolean;
+  '$txt-align-r'?: boolean;
 
-  ['col-w']?: string | number;
+  '$col-w'?: string | number;
 }
 
 export const TableRow = styled(Box)`
   display: table-row;
 `;
 
-export const TableCell = styled(Box)`
-    display: table-cell;
-   ${(props: TableCellProps) => props['valign-t'] && `vertical-align: top;`}
-   ${(props: TableCellProps) => props['valign-m'] && `vertical-align: middle;`}
-   ${(props: TableCellProps) => props['valign-b'] && `vertical-align: bottom;`}
-   ${(props: TableCellProps) => props['txt-align-l'] && `text-align: left;`}
-   ${(props: TableCellProps) => props['txt-align-c'] && `text-align: center;`}
-   ${(props: TableCellProps) => props['txt-align-r'] && `text-align: right;`}
-   ${(props: TableCellProps) =>
-     props['col-w'] && `column-width: ${getUnit(props['col-w'])};`}
+/* export const TableCell = styled(Box)<TableCellProps>`
+  display: table-cell;
+  ${(props) => props['valign-t'] && `vertical-align: top;`}
+  ${(props) => props['valign-m'] && `vertical-align: middle;`}
+   ${(props) => props['valign-b'] && `vertical-align: bottom;`}
+   ${(props) => props['txt-align-l'] && `text-align: left;`}
+   ${(props) => props['txt-align-c'] && `text-align: center;`}
+   ${(props) => props['txt-align-r'] && `text-align: right;`}
+   ${(props) =>
+    props['col-w'] && `column-width: ${getUnit(props['col-w'])};`}
 `;
+ */
+
+export const TableCell = styled(Box).attrs((props: TableCellProps) => ({
+  display: 'table-cell',
+  ...(props['$valign-t'] && {'vertical-align': 'top'}),
+  ...(props['$valign-m'] && {'vertical-align': 'middle'}),
+  ...(props['$valign-b'] && {'vertical-align': 'bottom'}),
+  ...(props['$txt-align-l'] && {'text-align': 'left'}),
+  ...(props['$txt-align-c'] && {'text-align': 'center'}),
+  ...(props['$txt-align-r'] && {'text-align': 'right'}),
+  ...(props['$col-w'] && {'column-width': getUnit(props['$col-w'])}),
+}))``;
 
 const Table = styled(Box)`
   display: table;
